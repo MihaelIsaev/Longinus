eb6cb1b14b31510d79d3451a9eab078ff07770cc
Mihael Isaev <isaev.mihael@gmail.com>
2022-04-09 10:46:57 +0300

    📦 Fix archiving issue, bump minimum iOS to 13

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "Longinus",
-    platforms: [.iOS(.v10)],
+    platforms: [.iOS(.v13)],
     products: [
         .library(
             name: "Longinus",
